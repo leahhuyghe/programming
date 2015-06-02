2b70bfb39e38c34df2e714b53673435ef861e345
Leah Huyghe <leahhuyghe@gmail.com>
2015-06-02 13:09:04 -0700

    Integrating Postgresql

---
+++ awesome_answers/app/models/question.rb
@@ -0,0 +1,4 @@
+#this is our model and it inherits from ActiveRecord::Base which gives us the ORM (Object Relational Mapping) features from the ActiveRecord gem.
+
+class Question < ActiveRecord::Base
+end
--- team_randomizer/app.rb
+++ team_randomizer/app.rb
@@ -1,15 +1,23 @@
 require "sinatra"
 
-get "/" do
+get "/randomizer" do
   erb :randomizer, layout: :application
-
 end
 
 post "/randomizer" do
   @names = params[:student_names]
-  @names.split(, )
+  @group_num = params[:group_number]
 
-  names = []
-  erb layout: :application
+  if @group_num.to_i > @names.split(", ").length
+    @result = "You require more teams than you have students listed"
+  else
+    @result = "still testing"
+  end
+
+  #make a group_number array
+  #push the randomized names into the group_number array for each element of the array
+  #print group number elements as "Group 1...n has student members:"
+  #print the subelements (the students) under each group number
+  erb :randomizer, layout: :application
 
 end
