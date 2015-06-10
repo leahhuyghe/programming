4488ca658dd33645a1024409d8085341daa011ea
Leah Huyghe <leahhuyghe@gmail.com>
2015-06-10 09:29:37 -0700

    Workin' on dat schema

--- awesome_answers/db/schema.rb
+++ awesome_answers/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150609213426) do
+ActiveRecord::Schema.define(version: 20150610162602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 20150609213426) do
 
   add_index "answers", ["question_id"], name: "index_answers_on_question_id", using: :btree
 
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text     "body"
     t.datetime "created_at", null: false
@@ -37,14 +43,17 @@ ActiveRecord::Schema.define(version: 20150609213426) do
   create_table "questions", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "view_count"
+    t.integer  "category_id"
   end
 
   add_index "questions", ["body"], name: "index_questions_on_body", using: :btree
+  add_index "questions", ["category_id"], name: "index_questions_on_category_id", using: :btree
   add_index "questions", ["title"], name: "index_questions_on_title", using: :btree
 
   add_foreign_key "answers", "questions"
   add_foreign_key "comments", "answers"
+  add_foreign_key "questions", "categories"
 end
