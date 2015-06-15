3f37d041e4b293af243d7e05f7d929abcef9008c
Leah Huyghe <leahhuyghe@gmail.com>
2015-06-15 14:17:43 -0700

    Destroying old copy of awesome_answers that is incomplete

--- awesome_answers/db/migrate/20150609213426_add_answer_references_to_comments.rb
+++
@@ -1,6 +0,0 @@
-class AddAnswerReferencesToComments < ActiveRecord::Migration
-
-  def change
-    add_reference :comments, :answer, index: true, foreign_key: true
-  end
-end
--- code_snippets/codesnipper/config/routes.rb
+++ code_snippets/codesnipper/config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 #  get 'welcome/index'
 
   root 'categories#show'
+
   resources :categories
   resources :snippets
 
