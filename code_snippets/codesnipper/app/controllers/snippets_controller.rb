2c27397fce691bdd0f6399aebf825dbcee6fa8c5
alwesam <alwesam@gmail.com>
2015-06-15 16:28:58 -0700

    add controllers code

--- code_snippets/codesnipper/app/controllers/snippets_controller.rb
+++ code_snippets/codesnipper/app/controllers/snippets_controller.rb
@@ -1,2 +1,52 @@
 class SnippetsController < ApplicationController
+  before_action :find_snippet, except: [:index, :new, :create]
+  def index
+    @snippets = Snippet.all
+  end
+
+  def new
+    @snippet = Snippet.new
+  end
+
+  def create
+    @snippet = Snippet.new(snippet_params)
+    if @snippet.save
+      redirect_to snippet_path(@snippet)
+    else
+      redirect :new
+    end
+    
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+    if @snippet.update(snippet_params) 
+      redirect_to snippet_path(@snippet)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @snippet.destroy
+    redirect_to snippets_path
+  end
+
+  private
+
+  def snippet_params
+       params.require(:snippet).permit([:title, :code, :category_id])
+  end
+
+  def find_snippet
+    @snippet = Snippet.find params[:id]
+  end
+
 end
